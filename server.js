9a82e408ae20fc818273e92762213238d28d2255
Shubham Londhe <shubhamlondhe63@gmail.com>
2024-04-02 12:07:22 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const mongoose = require('mongoose');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+// MongoDB setup
+mongoose.connect('mongodb://localhost/chat_app_db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Socket.io setup
+io.on('connection', socket => {
+  console.log('User connected:', socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
